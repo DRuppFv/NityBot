7b01f6022ec67139992ba3618e23a95553357dc5
DRuppFv <DRuppFv@gmail.com>
2022-07-05 18:08:06 -0300

    fixing

--- src/commands/general/wiki.rs
+++ src/commands/general/wiki.rs
@@ -3,6 +3,8 @@ use serenity::client::Context;
 use serenity::framework::standard::Args;
 use serenity::model::channel::Message;
 
+
+// refazer usando recursos mais inteligentes
 #[command]
 async fn wiki(ctx: &Context, msg: &Message) -> CommandResult {
     let mut arg = &msg.content;
@@ -10,9 +12,9 @@ async fn wiki(ctx: &Context, msg: &Message) -> CommandResult {
     if arg.contains("!f wiki") {
         arg.replace("!f wiki", "");
     } else if arg.contains("!fwiki") {  
-        arg.replace("!f wiki", "");
+        arg.replace("!fwiki", "");
     }
 
-    let wiki_url = format!("https://pt.wikipedia.org/wiki/{}", arg);
+    let wiki_url = format!("https://pt.wikipedia.org/wiki/{}", arg.replace(" ", "_"));
     Ok(())
 }
