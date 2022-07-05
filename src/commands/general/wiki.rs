284a50aa7c90b149b1877af8e624ee73ea4eccc2
DRuppFv <DRuppFv@gmail.com>
2022-07-05 16:00:37 -0300

    basic concept

--- src/commands/general/wiki.rs
+++ src/commands/general/wiki.rs
@@ -4,7 +4,15 @@ use serenity::framework::standard::Args;
 use serenity::model::channel::Message;
 
 #[command]
-async fn wiki(ctx: &Context, msg: &Message, arg: Args) -> CommandResult {
-    
+async fn wiki(ctx: &Context, msg: &Message) -> CommandResult {
+    let mut arg = &msg.content;
+
+    if arg.contains("!f wiki") {
+        arg.replace("!f wiki", "");
+    } else if arg.contains("!fwiki") {  
+        arg.replace("!f wiki", "");
+    }
+
+    let wiki_url = format!("https://pt.wikipedia.org/wiki/{}", arg);
     Ok(())
 }
