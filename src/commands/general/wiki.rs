99a44de70b4e81e92eb3d18e683626bd432a05d6
DRuppFv <DRuppFv@gmail.com>
2022-07-05 15:37:40 -0300

    Add bot client

--- src/commands/general/wiki.rs
+++ src/commands/general/wiki.rs
@@ -1,6 +1,10 @@
 use serenity::framework::standard::{macros::command, CommandResult};
+use serenity::client::Context;
+use serenity::framework::standard::Args;
+use serenity::model::channel::Message;
 
 #[command]
-async fn wiki() -> CommandResult {
+async fn wiki(ctx: &Context, msg: &Message, arg: Args) -> CommandResult {
+    
     Ok(())
 }
