419b2e02116f02fb10d58bae2b3cd4ec56152b71
DRuppFv <DRuppFv@gmail.com>
2022-07-08 23:46:46 -0300

    Add wiki-client to wiki command

--- src/commands/general/wiki.rs
+++ src/commands/general/wiki.rs
@@ -8,10 +8,24 @@ async fn wiki(ctx: &Context, msg: &Message) -> CommandResult {
         let matches = command.try_get_matches_from(
         msg.content.to_clap_command("!f".to_string().clone(), "wiki"))?;
 
-        let wiki = matches.value_of("wiki_subject").unwrap();
-        println!("{}", wiki);
-        
-        msg.reply(ctx, format!("https://pt.wikipedia.org/wiki/{}", wiki.replace(" ", ""))).await?;
+        let wiki = matches.value_of("wiki_subject").unwrap().replacen(" ", "", 1);
+
+        let wiki_client = wikipedia::Wikipedia {
+            client: <wikipedia::http::default::Client>::default(),
+            pre_language_url: String::from("https://"),
+            post_language_url: String::from(".wikipedia.org/w/api.php"),
+            language: String::from("pt"),
+            search_results: 5,
+            images_results: String::from("min"),
+            links_results: String::from("min"),
+            categories_results: String::from("min")
+        };
+
+        let result = &wiki_client.search(&wiki).unwrap()[0];
+
+        msg.reply(
+            ctx, format!("https://pt.wikipedia.org/wiki/{}", result.replace(" ", "_"))
+        ).await?;
 
         Ok(())
     })
