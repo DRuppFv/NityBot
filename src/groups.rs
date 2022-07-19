64e187cf03fa3d5e2d3ba8f0ee4cdab4213c3817
DRuppFv <DRuppFv@gmail.com>
2022-07-19 01:04:39 -0300

    Rename command

--- src/groups.rs
+++ src/groups.rs
@@ -1,19 +1,19 @@
 use crate::commands::general::{
+    lang::LANG_COMMAND,
     langlist::LANGLIST_COMMAND,
 };
 
 use crate::commands::wiki::{
     wiki::WIKI_COMMAND,
     wikihelp::WIKIHELP_COMMAND,    
-    wikilang::WIKILANG_COMMAND,
 };
 
 use serenity::framework::standard::macros::group;
 
 #[group]
-#[commands(langlist)]
+#[commands(lang, langlist)]
 struct General;
 
 #[group]
-#[commands(wiki, wikihelp, wikilang)]
+#[commands(wiki, wikihelp)]
 struct Wiki;
