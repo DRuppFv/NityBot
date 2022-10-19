d3db69ea9be1cd2cd6129c7953bc87d2a9e70e6a
DRuppFv <DRuppFv@gmail.com>
2022-10-19 18:24:17 -0300

    Update wikiuser command answer with icon and name

--- src/commands/wiki/wikiuser.rs
+++ src/commands/wiki/wikiuser.rs
@@ -18,7 +18,7 @@ async fn wikiuser(ctx: &Context, msg: &Message) -> CommandResult {
         return Ok(());
     }
     
-    let _user = if let serenity::model::mention::Mention::User(UserId(id)) = mention.unwrap() {
+    let user = if let serenity::model::mention::Mention::User(UserId(id)) = mention.unwrap() {
         UserId::from(id).to_user(&ctx.http).await?
     } else {
         msg
@@ -28,20 +28,35 @@ async fn wikiuser(ctx: &Context, msg: &Message) -> CommandResult {
         return Ok(());
     };
 
+
+    let user_nick_in = user.nick_in(&ctx.http, msg.guild_id.unwrap()).await
+        .unwrap_or(user.name.to_string());
+        
+    let user_avatar = &user.avatar_url().unwrap();
+
     let _ = msg
         .channel_id
         .send_message(&ctx.http, |m| {
             m.content(format!(
-                "content"
+                ""                       //content
             ))
             .embed(|e| {
-                e.title(format!("title"))
-                    .description(format!(
-                        "desc"
-                    ))
-                    .fields(vec![("fields", "field 1", true)])
-                    .footer(|f| f.text(format!("")))
-                    .colour(Colour::from_rgb(91, 8, 199))
+                e.title(format!(""))                   //title
+                .author(|a: &mut serenity::builder::CreateEmbedAuthor|
+                a.icon_url(user_avatar)
+                .name(user_nick_in))
+                .thumbnail(user_avatar)
+                .description(format!(
+                    ""                      //description
+                ))
+                .fields(vec![
+                    ("", "", true),   //fields
+                    ("", "", true)
+                    ])
+                .footer(|f| 
+                f.icon_url(user_avatar)
+                .text(user.tag()))
+                .colour(Colour::from_rgb(91, 8, 199))
             })
         })
         .await?;
